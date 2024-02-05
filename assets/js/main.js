7451a60a771be85188200c6696d35a566ac4340a
Maiara Moraes <maiara.ctt@gmail.com>
2024-02-05 18:03:23 -0300

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,60 @@
 // requisição http via javascript
 // fetch API
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => {
-        return ` <li class="type">${typeSlot.type.name}</li> `
-    })
-};
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 8;
+let offset = 0;
+
+
+// function convertPokemonTypesToLi(pokemonTypes) {
+//     return pokemonTypes.map((typeSlot) => {
+//         return ` <li class="type">${typeSlot.type.name}</li> `
+//     })
+// };
 
 
 function convertPokemonToLi(pokemon) {
 return `
 
-<li class="pokemon">
-<span class="number">${pokemon.order}</span>
-<span class="'name">${pokemon.name}</span>
-<div class="detail">
-    <ol class="types">
-    ${convertPokemonTypesToLi(pokemon.types).join('')}
-    </ol>
-    <img src="${pokemon.sprites.other.dream_world.front_default}" 
-    alt="${pokemon.name}">
+<li class="pokemon ${pokemon.type}">
+    <span class="number">
+    ${pokemon.number}
+    </span>
+
+    <span class="name">
+    ${pokemon.name}
+    </span>
+
+    <div class="detail">
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+        </ol>
+    
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
     
     </div>
     </li>
 `};
 
-const pokemonList = document.getElementById('pokemonList');
+function loadPokemonsItens(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
 
-pokeApi.getPokemons().then((pokemons = []) => {
+        const newList = pokemons.map((pokemon) => {
 
-    const newList = pokemons.map((pokemon) => {
-        return convertPokemonToLi(pokemon)
+
+            return convertPokemonToLi(pokemon);
+        })
+        const newHTML = newList.join('');
+
+        pokemonList.innerHTML += newHTML;
     })
-    const newHTML = newList.join('');
-    pokemonList.innerHTML += newHTML;
-})
+}
 
-// JEITO MAIS SIMPLES DE FAZER:
+loadPokemonsItens(offset, limit);
 
-// const pokemonList = document.getElementById('pokemonList');
-// pokeApi.getPokemons().then((pokemons = []) => {
-//     pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
-// })
-
- 
+ loadMoreButton.addEventListener('click', () =>{
+    offset += limit;
+    loadPokemonsItens(offset, limit);
+ })
 
 
