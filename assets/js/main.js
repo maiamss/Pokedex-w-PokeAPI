2fa5bff191f1761ff31f585c1cde2b4bbd5107d3
Maiara Moraes <maiara.ctt@gmail.com>
2024-02-04 21:25:50 -0300

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,42 @@
 // requisição http via javascript
 // fetch API
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+return `
 
-fetch(url)//fetch retorna uma promisse
-.then((response) =>  response.json()) //encadeamento de then's
-.then((jsonBody) => console.log(jsonBody))  //o que vai p segundo then é o retorno do primeiro
-.catch((error) => console.log(error))
+<li class="pokemon">
+<span class="number">#001</span>
+<span class="'name">${pokemon.name}</span>
+<div class="detail">
+    <ol class="types">
+        <li class="type">grass</li>
+        <li class="type">poison</li>
+    </ol>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+    alt="${pokemon.name}">
+    
+    </div>
+    </li>
+`};
 
+const pokemonList = document.getElementById('pokemonList');
 
+pokeApi.getPokemons().then((pokemons = []) => {
 
+    const nerList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon)
+    })
+    const newHTML = nerList.join('');
+    pokemonList.innerHTML += newHTML;
+})
+
+// JEITO MAIS SIMPLES DE FAZER:
+
+// const pokemonList = document.getElementById('pokemonList');
+// pokeApi.getPokemons().then((pokemons = []) => {
+//     pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+// })
+
+ 
 
 
